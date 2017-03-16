af3cc2786342810a9ba0d2cc86579855a80237f8
Sergey Sova <i.am@lestad.net>
2017-03-16 14:03:39 +0300

    Change module generation command name

--- bin/adr.js
+++ bin/adr.js
@@ -7,6 +7,7 @@ const caporal = require('caporal')
 caporal
   .version(Package.version)
   .description('Generate your Atomic components for React')
+  // .option('--dummy', 'Only show what will br changed. Do not change file system.')
 
 caporal
   .command('init', 'Generate base atomic structure for your project')
@@ -17,19 +18,20 @@ caporal
 const typesAvailable = ['atom', 'molecule', 'organism', 'template', 'page']
 
 caporal
+  .command('module', 'Generate module structure')
+  .argument('<name>', 'CamelCased name of new module')
+  .action((args, options) => {
+    console.log('NEW MODULE', args, options)
+  })
+
+caporal
   .command(`new`, 'Create new component')
   .argument('<type>', 'One of: atom, molecule, organism, template, page', typesAvailable)
   .complete(() => typesAvailable)
   .argument('<name>', `CamelCased name of your component, can have / to set module name`)
+  .option('--random', ';aasd')
   .action((args, options, logger) => {
     console.log(`NEW`, args, options)
-  })
-
-caporal
-  .command('new module', 'Generate module structure')
-  .argument('<name>', 'CamelCased name of new module')
-  .action((args, options) => {
-    console.log('NEW MODULE', args, options)
   })
 
 caporal.parse(process.argv)
